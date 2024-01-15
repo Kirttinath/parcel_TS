b377c24a32ed7f0645260b45b56e9358874cc63f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-15 18:31:18 +0530

    bundler created with typescript

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,3 +1,11 @@
-console.log("Hello Kirtti");
+const buttonElem = document.querySelector(".clickme");
+const bodyElem = document.body;
+let isWhite = false;
+buttonElem.addEventListener("click", ()=>{
+    console.log("I clicked.");
+    if (isWhite) bodyElem.style.backgroundColor = "";
+    else bodyElem.style.backgroundColor = "#CEDEBD";
+    isWhite = !isWhite;
+});
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello Kirtti");
+const buttonElem = document.querySelector(".clickme") as HTMLButtonElement;
+
+const bodyElem: HTMLElement = document.body;
+
+let isWhite: boolean = false;
+
+buttonElem.addEventListener("click", () => {
+  console.log("I clicked.");
+
+  if (isWhite) {
+    bodyElem.style.backgroundColor = "";
+  } else {
+    bodyElem.style.backgroundColor = "#CEDEBD";
+  }
+  isWhite = !isWhite;
+});
